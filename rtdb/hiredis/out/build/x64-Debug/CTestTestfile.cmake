d84661163108b707c7f7774c8afb6699b2337546
hsunekichi <hsunekichi@gmail.com>
2024-09-03 19:59:12 +0200

    Deleted unused files

--- rtdb/hiredis/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/hmateo/Desktop/servidorRedis/hiredis
-# Build directory: C:/Users/hmateo/Desktop/servidorRedis/hiredis/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(hiredis-test "C:/Users/hmateo/Desktop/servidorRedis/hiredis/test.sh")
-set_tests_properties(hiredis-test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/hmateo/Desktop/servidorRedis/hiredis/CMakeLists.txt;247;ADD_TEST;C:/Users/hmateo/Desktop/servidorRedis/hiredis/CMakeLists.txt;0;")
